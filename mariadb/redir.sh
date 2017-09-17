c3f53caf7332702e84002ac1f0d13e7a1f388d37
Evgeniy Sinev <es@payneteasy.com>
2017-09-17 19:35:37 +0300

    Switched to use socat because redir create too many processes

--- mariadb/redir.sh
+++ mariadb/redir.sh
@@ -3,4 +3,4 @@
 DEFAULT_IP=`/sbin/ip route get 1 | awk '{print $NF;exit}'`
 
 echo "Redirecting from $DEFAULT_IP:3306 to 127.0.0.1:3306 ..."
-redir $DEFAULT_IP:3306 127.0.0.1:3306
+(socat TCP4-LISTEN:3306,fork,bind=$DEFAULT_IP TCP4:127.0.0.1:3306) &
