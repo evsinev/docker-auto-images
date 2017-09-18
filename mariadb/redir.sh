08ea968bf0d95ecb0770f29254d0dc3920fd3939
Evgeniy Sinev <es@payneteasy.com>
2017-09-18 12:55:01 +0300

    Redirect from 3306 to mysql socket file

--- mariadb/redir.sh
+++ mariadb/redir.sh
@@ -2,5 +2,5 @@
 
 DEFAULT_IP=`/sbin/ip route get 1 | awk '{print $NF;exit}'`
 
-echo "Redirecting from $DEFAULT_IP:3306 to 127.0.0.1:3306 ..."
-(socat TCP4-LISTEN:3306,fork,bind=$DEFAULT_IP TCP4:127.0.0.1:3306) &
+echo "Redirecting from $DEFAULT_IP:3306 to /var/run/mysqld/mysqld.sock ..."
+(socat TCP4-LISTEN:3306,fork,bind=$DEFAULT_IP UNIX-CONNECT:/var/run/mysqld/mysqld.sock) &
