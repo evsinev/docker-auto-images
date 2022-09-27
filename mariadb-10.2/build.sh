0e50109610beac7270af59a9a0be0369b316632e
Evgeniy Sinev <es@payneteasy.com>
2022-09-27 17:35:40 +0300

    Added mariadb 10.2.44

---
+++ mariadb-10.2/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -eux
+
+docker build -t payneteasy/mariadb-m1:10.2.44 .
---
+++ mariadb-10.2/redir.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo
+echo
+echo
+echo "Running runit.sh ..."
+echo
+echo
+echo
+
+/usr/bin/runsvdir /service &
