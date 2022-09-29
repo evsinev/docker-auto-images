76ef32a5d2a80939f467fe0ef308f6aa923aa7e9
esinev <es@payneteasy.com>
2022-09-29 05:37:56 +0300

    Fixed mariadb for startup with gitlab-runner

--- mariadb-10.2/build.sh
+++ mariadb-10.2/build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -eux
 
-docker build -t payneteasy/mariadb-m1:10.2.44 .
+wget https://github.com/evsinev/network-proxy/releases/download/1.0-1/network-proxy-server -O network-proxy-server
+#cp /home/graal/src/build-network-proxy/network-proxy-server-1.0-SNAPSHOT ./network-proxy-server
+
+docker build -t payneteasy/mariadb:10.2.44-4 .
